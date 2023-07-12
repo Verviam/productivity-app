d278c1ccea8fce13938b3a9089d55d9db265bee9
Evan Gao <gaoevan37@gmail.com>
2023-07-12 12:01:54 -0400

    main

--- Main Menu/main.py
+++ Main Menu/main.py
@@ -1,4 +1,4 @@
-import mainGui, Schedule.schedule as schedule, Tasks.toDoList as toDoList 
+import mainGui as mainGui, Schedule.schedule as schedule, ToDo.toDoList as todo
 
 
 print("Productivity: Get Stuff Done \n Menu: To-Do List, Habit Tracker, My Schedule, ")
