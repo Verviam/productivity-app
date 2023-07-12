6879f5bfd0ca15124ff3b36e678d5d35b0015f7c
Evan Gao <gaoevan37@gmail.com>
2023-07-12 11:57:11 -0400

    made folders and organized

--- Main Menu/main.py
+++ Main Menu/main.py
@@ -1,4 +1,4 @@
-import mainGui, schedule, toDoList 
+import mainGui, Schedule.schedule as schedule, Tasks.toDoList as toDoList 
 
 
 print("Productivity: Get Stuff Done \n Menu: To-Do List, Habit Tracker, My Schedule, ")
