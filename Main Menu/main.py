b229bc2f70de78ccb8e9738e91b5f392fdf5c66d
Evan Gao <gaoevan37@gmail.com>
2023-07-13 09:00:12 -0400

    Update main.py

--- Main Menu/main.py
+++ Main Menu/main.py
@@ -2,3 +2,6 @@ import mainGui as main, Schedule.schedule as schedule, ToDo.toDoList as todo
 
 #One of the best ways to get things done is by creating habits and systems. Productivity helps you get things done by using scientifically proven ways to help you create habits to manage your schedule and todo lists and enjoy doing work
 print("Productivity: Get Stuff Done \n Menu: To-Do List, Habit Tracker, My Schedule, ")
+
+# press button and switch to new file
+# def 
