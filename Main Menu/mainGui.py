5ecf07a03dde1c96b0f869990e58fbae2a8106a4
Evan Gao <gaoevan37@gmail.com>
2023-07-18 08:28:10 -0400

    a

--- Main Menu/mainGui.py
+++ Main Menu/mainGui.py
@@ -7,15 +7,19 @@ import customtkinter as ctk
 root = Tk(className='Productivity') #create window and name
 root.geometry("1080x720") # set window size
 root.configure(bg='white') # set window color
-
 # icon
 Image_icon = PhotoImage(file="productivity_icon.png")
 root.iconphoto(False, Image_icon)
+
 
 #taskbar
 taskbar_border_color = Frame(root, background="black")
 taskbar = Listbox(root, selectbackground='black', bg='#073B3A', font=('Helvetica', 12), height=50, width=5)
 taskbar.place(x=-1, y=0)
+#home
+homeImg = ImageTk.PhotoImage(Image.open("home.png"))
+home = Label(root, image=homeImg, bg="white")
+home.pack(anchor=NW)
 
 # change the background color of side menu and main frame of app
 
@@ -28,9 +32,5 @@ scroller.place(x=260, y=80, height=232)
 tasks.config(yscrollcommand=scroller.set)
 tasks.place(x=35, y=80)
 
-#home
-# homeImg = ImageTk.PhotoImage(Image.open("home.png"))
-# home = Label(root, image=homeImg, bg="white")
-# home.pack(anchor=NW)
 
 root.mainloop()
