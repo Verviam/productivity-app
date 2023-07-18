f7a2f93d2520ccd1d2ea90bf7b7d654f0ccfdb32
Evan Gao <gaoevan37@gmail.com>
2023-07-18 09:40:31 -0400

    image folder

--- Main Menu/mainGui.py
+++ Main Menu/mainGui.py
@@ -8,7 +8,7 @@ root = Tk(className='Productivity') #create window and name
 root.geometry("1080x720") # set window size
 root.configure(bg='white') # set window color
 # icon
-Image_icon = PhotoImage(file="productivity_icon.png")
+Image_icon = PhotoImage(file="Image/productivity_icon.png")
 root.iconphoto(False, Image_icon)
 
 
@@ -18,12 +18,12 @@ taskbar = Listbox(root, selectbackground='black', bg='#073B3A', fg="#073B3A", fo
 taskbar.place(x=-2, y=0)
 
 #home
-homeImg = ImageTk.PhotoImage(Image.open("home.png"))
+homeImg = ImageTk.PhotoImage(Image.open("Image/home.png"))
 home = Label(root, image=homeImg, bg="#073B3A")
 home.place(x=-1, y=0)
 
 #home
-todolistImg = ImageTk.PhotoImage(Image.open("todolist_img.png"))
+todolistImg = ImageTk.PhotoImage(Image.open("Image/todolist_img.png"))
 todolist = Label(root, image=todolistImg, bg="#073B3A")
 todolist.place(x=-1, y=55)
 
