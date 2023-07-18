88d532a714d981113259924786efa71d5fa0b8c9
Twilight9676 <leoyang9876@gmail.com>
2023-07-18 08:34:38 -0400

    e

--- Main Menu/mainGui.py
+++ Main Menu/mainGui.py
@@ -14,23 +14,23 @@ root.iconphoto(False, Image_icon)
 
 #taskbar
 taskbar_border_color = Frame(root, background="black")
-taskbar = Listbox(root, selectbackground='black', bg='#073B3A', font=('Helvetica', 12), height=50, width=5)
-taskbar.place(x=-1, y=0)
+taskbar = Listbox(root, selectbackground='black', bg='#073B3A', fg="#073B3A", font=('Helvetica', 12), height=50, width=7)
+taskbar.place(x=-2, y=0)
 
 #home
 homeImg = ImageTk.PhotoImage(Image.open("home.png"))
 home = Label(root, image=homeImg, bg="#073B3A")
-home.pack(anchor=NW)
+home.place(x=-1, y=0)
 
 # change the background color of side menu and main frame of app
 
-Label(root, text="To Do List", bg="white", font=("Arial", 15), wraplength=300).place(x=50, y=30.5) #Edit font size and wrap length and place later
+Label(root, text="To Do List", bg="white", font=("Arial", 15), wraplength=300).place(x=150, y=30.5) #Edit font size and wrap length and place later
 
 tasks = Listbox(root, selectbackground='Gold', bg='Silver', font=('Helvetica', 12), height=12, width=25)
 scroller = Scrollbar(root, orient=VERTICAL, command=tasks.yview)
-scroller.place(x=260, y=80, height=232)
+scroller.place(x=360, y=80, height=232)
 
 tasks.config(yscrollcommand=scroller.set)
-tasks.place(x=35, y=80)
+tasks.place(x=135, y=80)
 
 root.mainloop()
