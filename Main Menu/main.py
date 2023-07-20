d1e21b2f8e3418399fecf659e0cb28bfeb5082c8
Evan Gao <gaoevan37@gmail.com>
2023-07-20 09:11:01 -0400

    Update main.py

--- Main Menu/main.py
+++ Main Menu/main.py
@@ -78,9 +78,13 @@ class root(tk.Tk):
 
         root = tk.Frame(self)
         root.pack()
+        
+        # window setup
+        root = tk.Tk(className='Productivity') #create window and name
+        root.geometry("1080x720") # set window size
+        root.configure(bg='#0B6E4F') # set window color
 
-        
-        
+
         self.frames = {}
         for P in (homePage, toDoList, habits, schedule, notes):
             page = P(root, self)
@@ -89,77 +93,74 @@ class root(tk.Tk):
 
         self.show_frame(homePage)
 
-        def show_frame(self, page):
-            frame = self.frames[page]
-            frame.tkraise()
+    def show_frame(self, page):
+        frame = self.frames[page]
+        frame.tkraise()
 
 
-# window setup
-root = tk.Tk(className='Productivity') #create window and name
-root.geometry("1080x720") # set window size
-root.configure(bg='#0B6E4F') # set window color
 
-# icon
-Image_icon = tk.PhotoImage(file="Image/productivity_icon.png")
-root.iconphoto(False, Image_icon)
+# # icon
+# Image_icon = tk.PhotoImage(file="Image/productivity_icon.png")
+# root.iconphoto(False, Image_icon)
 
 
-#taskbar 
-taskbar = tk.Listbox(root, selectbackground='black', bg='#073B3A', fg="#073B3A", font=('Helvetica', 12), height=60, width=8)
-taskbar.place(x=-2, y=-3)
+# #taskbar 
+# taskbar = tk.Listbox(root, selectbackground='black', bg='#073B3A', fg="#073B3A", font=('Helvetica', 12), height=60, width=8)
+# taskbar.place(x=-2, y=-3)
 
-#logo
-productivityIcon = ImageTk.PhotoImage(Image.open("Image/productivity_icon.png"))
-productivity = tk.Label(root, image=productivityIcon, bg="#073B3A")
-productivity.place(x=8, y=5)
+# #logo
+# productivityIcon = ImageTk.PhotoImage(Image.open("Image/productivity_icon.png"))
+# productivity = tk.Label(root, image=productivityIcon, bg="#073B3A")
+# productivity.place(x=8, y=5)
 
-#home
-homeImg = ImageTk.PhotoImage(Image.open("Image/home_img.png"))
-home = tk.Label(root, image=homeImg, bg="#073B3A")
-home.place(x=16, y=56)
+# #home
+# homeImg = ImageTk.PhotoImage(Image.open("Image/home_img.png"))
+# home = tk.Label(root, image=homeImg, bg="#073B3A")
+# home.place(x=16, y=56)
 
-#todolist
-todolistImg = ImageTk.PhotoImage(Image.open("Image/todolist_img.png"))
-todolist = tk.Label(root, image=todolistImg, bg="#073B3A")
-todolist.place(x=int(14.7), y=100)
+# #todolist
+# todolistImg = ImageTk.PhotoImage(Image.open("Image/todolist_img.png"))
+# todolist = tk.Label(root, image=todolistImg, bg="#073B3A")
+# todolist.place(x=int(14.7), y=100)
 
-#schedule
-scheduleImg = ImageTk.PhotoImage(Image.open("Image/schedule_img.png"))
-schedule = tk.Label(root, image=scheduleImg, bg="#073B3A")
-schedule.place(x=int(14.7), y=150)
+# #schedule
+# scheduleImg = ImageTk.PhotoImage(Image.open("Image/schedule_img.png"))
+# schedule = tk.Label(root, image=scheduleImg, bg="#073B3A")
+# schedule.place(x=int(14.7), y=150)
 
-#habits
-habitsImg = ImageTk.PhotoImage(Image.open("Image/habits_img.png"))
-habits = tk.Label(root, image=habitsImg, bg="#073B3A")
-habits.place(x=int(14.7), y=201)
+# #habits
+# habitsImg = ImageTk.PhotoImage(Image.open("Image/habits_img.png"))
+# habits = tk.Label(root, image=habitsImg, bg="#073B3A")
+# habits.place(x=int(14.7), y=201)
 
-#notes
-notesImg = ImageTk.PhotoImage(Image.open("Image/notes_img.png"))
-notes = tk.Label(root, image=notesImg, bg="#073B3A")
-notes.place(x=19, y=251)
+# #notes
+# notesImg = ImageTk.PhotoImage(Image.open("Image/notes_img.png"))
+# notes = tk.Label(root, image=notesImg, bg="#073B3A")
+# notes.place(x=19, y=251)
 
-#arrows
-arrowsImg = ImageTk.PhotoImage(Image.open("Image/right_arrows_img.png"))
-arrows = tk.Label(root, image=arrowsImg, bg="#073B3A")
-arrows.place(x=23, y=930)
+# #arrows
+# arrowsImg = ImageTk.PhotoImage(Image.open("Image/right_arrows_img.png"))
+# arrows = tk.Label(root, image=arrowsImg, bg="#073B3A")
+# arrows.place(x=23, y=930)
 
-#settings
-settingsImg = ImageTk.PhotoImage(Image.open("Image/settings_img.png"))
-settings = tk.Label(root, image=settingsImg, bg="#073B3A")
-settings.place(x=19, y=970)
+# #settings
+# settingsImg = ImageTk.PhotoImage(Image.open("Image/settings_img.png"))
+# settings = tk.Label(root, image=settingsImg, bg="#073B3A")
+# settings.place(x=19, y=970)
 
-#timer
-timerImg = ImageTk.PhotoImage(Image.open("Image/timer_img.png"))
-timer = tk.Label(root, image=timerImg, bg="#073B3A")
-timer.place(x=13, y=298)
+# #timer
+# timerImg = ImageTk.PhotoImage(Image.open("Image/timer_img.png"))
+# timer = tk.Label(root, image=timerImg, bg="#073B3A")
+# timer.place(x=13, y=298)
 
-# change the background color of side menu and root frame of app
+# # change the background color of side menu and root frame of app
 
-tk.Label(root, text="Hello Muscle Cow", bg="#0B6E4F", fg ="#21D375", font=("Helvetica", 25), wraplength=300).place(x=85, y=10) #Edit font size and wrap length and place later
+# tk.Label(root, text="Hello Muscle Cow", bg="#0B6E4F", fg ="#21D375", font=("Helvetica", 25), wraplength=300).place(x=85, y=10) #Edit font size and wrap length and place later
 
-tasks = tk.Listbox(root, selectbackground='Gold', bg='Silver', font=('Helvetica', 12), height=12, width=25)
-tasks.place(x=135, y=80)
+# tasks = tk.Listbox(root, selectbackground='Gold', bg='Silver', font=('Helvetica', 12), height=12, width=25)
+# tasks.place(x=135, y=80)
 
 
-#root = root()
+root = root()
 root.mainloop()
+
