bd80833e2809adc4f0ed884cbd5fe35bb379ab5d
Twilight9676 <leoyang9876@gmail.com>
2023-07-25 09:01:10 -0400

    e

--- Main Menu/main.py
+++ Main Menu/main.py
@@ -88,49 +88,49 @@ optionsMenu = tk.Frame(root, bg='#073B3A')
 
 # Home Image Button
 homeImg = ImageTk.PhotoImage(Image.open("Image/home_img.png"))
-homeButton = tk.Button(optionsMenu, image = homeImg, borderwidth=0, highlightthickness=0, command=lambda: indicator(homeIndicate, homePage))
+homeButton = tk.Button(optionsMenu, image = homeImg, borderwidth=0, highlightthickness=0, command=lambda: indicator(homeIndicate, homePage), activebackground="#073B3A")
 homeButton.place(x=10, y=50)
 homeIndicate = tk.Label(optionsMenu, text='', bg = '#073B3A')
 homeIndicate.place(x=3, y=50, width=5, height=40)
 
 # To Do List Button
 todoListImg = ImageTk.PhotoImage(Image.open("Image/todolist_img.png"))
-todoListButton = tk.Button(optionsMenu, image = todoListImg,borderwidth=0, highlightthickness=0, command=lambda: indicator(todoListIndicate, toDoPage))
+todoListButton = tk.Button(optionsMenu, image = todoListImg,borderwidth=0, highlightthickness=0, command=lambda: indicator(todoListIndicate, toDoPage), activebackground="#073B3A")
 todoListButton.place(x=10, y=105)
 todoListIndicate = tk.Label(optionsMenu, text='', bg = '#073B3A')
 todoListIndicate.place(x=3, y=105, width=5, height=40)
 
 # Schedule Button
 scheduleImg = ImageTk.PhotoImage(Image.open("Image/schedule_img.png"))
-scheduleButton = tk.Button(optionsMenu, image = scheduleImg, borderwidth=0, highlightthickness=0, command=lambda: indicator(scheduleIndicate, schedulePage))
+scheduleButton = tk.Button(optionsMenu, image = scheduleImg, borderwidth=0, highlightthickness=0, command=lambda: indicator(scheduleIndicate, schedulePage), activebackground="#073B3A")
 scheduleButton.place(x=10, y=160)
 scheduleIndicate = tk.Label(optionsMenu, text='', bg = '#073B3A')
 scheduleIndicate.place(x=3, y=160, width=5, height=40)
 
 # Habits Button
 habitsImg = ImageTk.PhotoImage(Image.open("Image/habits_img.png"))
-habitsButton = tk.Button(optionsMenu, image = habitsImg, borderwidth=0, highlightthickness=0, command=lambda: indicator(habitsIndicate, habitsPage))
+habitsButton = tk.Button(optionsMenu, image = habitsImg, borderwidth=0, highlightthickness=0, command=lambda: indicator(habitsIndicate, habitsPage), activebackground="#073B3A")
 habitsButton.place(x=10, y=215)
 habitsIndicate = tk.Label(optionsMenu, text='', bg = '#073B3A')
 habitsIndicate.place(x=3, y=215, width=5, height=40)
 
 # Notes Button
 notesImg = ImageTk.PhotoImage(Image.open("Image/notes_img.png"))
-notesButton = tk.Button(optionsMenu, image = notesImg, borderwidth=0, highlightthickness=0, command=lambda: indicator(notesIndicate, notesPage))
+notesButton = tk.Button(optionsMenu, image = notesImg, borderwidth=0, highlightthickness=0, command=lambda: indicator(notesIndicate, notesPage), activebackground="#073B3A")
 notesButton.place(x=10, y=270)
 notesIndicate = tk.Label(optionsMenu, text='', bg = '#073B3A')
 notesIndicate.place(x=3, y=270, width=5, height=40)
 
 # Timer Button
 timerImg = ImageTk.PhotoImage(Image.open("Image/timer_img.png"))
-timerButton = tk.Button(optionsMenu, image = timerImg, borderwidth=0, highlightthickness=0, command=lambda: indicator(timerIndicate, timerPage))
+timerButton = tk.Button(optionsMenu, image = timerImg, borderwidth=0, highlightthickness=0, command=lambda: indicator(timerIndicate, timerPage), activebackground="#073B3A")
 timerButton.place(x=10, y=325)
 timerIndicate = tk.Label(optionsMenu, text='', bg = '#073B3A')
 timerIndicate.place(x=3, y=325, width=5, height=40)
 
 # Settings Button
 settingsImg = ImageTk.PhotoImage(Image.open("Image/settings_img.png"))
-settingsButton = tk.Button(optionsMenu, image = settingsImg, borderwidth=0, highlightthickness=0, bg = "#073B3A", command=lambda: indicator(settingsIndicate, settingsPage))
+settingsButton = tk.Button(optionsMenu, image = settingsImg, borderwidth=0, highlightthickness=0, bg = "#073B3A", command=lambda: indicator(settingsIndicate, settingsPage), activebackground="#073B3A")
 settingsButton.place(x=12, y=700)
 settingsIndicate = tk.Label(optionsMenu, text='', bg = '#073B3A')
 settingsIndicate.place(x=3, y=700, width=5, height=40)
